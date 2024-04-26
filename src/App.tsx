3f12dc3d5999c4f2c0b9711e8665b04279b839f0
Julio Mayén <mayenrosil@gmail.com>
2024-04-26 09:15:29 -0600

    estilizado de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -20,34 +20,45 @@ const App: React.FC = () => {
   const [codeText, setCodeText] = useState<string>("");
 
   const [listadoPalabras, setListadoPalabras] = useState<Array<Palabra>>([]);
-  
+
   const [listadoAgrupado, setListadoAgrupado] = useState<Array<Array<Palabra>>>([]);
+
+  const [analizarLexico, setAnalizarLexico] = useState<boolean>(false);
 
   const analisisLexico = (codigoPlano: string) => {
     setListadoPalabras(validadorLexico(codigoPlano));
-    
+
     agrupar(validadorLexico(codigoPlano));
   }
 
   const agrupar = (listadoPalabras: Palabra[]): Palabra[][] => {
     const nuevoArray: Palabra[][] = listadoPalabras.reduce((acumulador: Palabra[][], objeto) => {
-        const indice = objeto.numeroLinea - 1; // Restamos 1 para evitar problemas con los índices
-        acumulador[indice] = acumulador[indice] || []; // Creamos el subarray si no existe
-        acumulador[indice].push(objeto);
-        return acumulador;
+      const indice = objeto.numeroLinea - 1; // Restamos 1 para evitar problemas con los índices
+      acumulador[indice] = acumulador[indice] || []; // Creamos el subarray si no existe
+      acumulador[indice].push(objeto);
+      return acumulador;
     }, []);
-    
+
     setListadoAgrupado(nuevoArray)
+    setAnalizarLexico(true)
     return nuevoArray;
-}
+  }
 
   return (
     <div className='main-container container'>
       <h1>Analizador de Código</h1>
-      <FileInput setCodeText={setCodeText} />
-      <CodeInput recibirCodigo={codeText} validadorLexico={(codigoPlano: string) => analisisLexico(codigoPlano)} setCodeText={setCodeText} />
-      <TablesRender listadoPalabras={listadoPalabras} />
-      <PrettyCode listadoAgrupado={listadoAgrupado} />
+      {!analizarLexico ?
+        <React.Fragment>
+          <FileInput setCodeText={setCodeText} />
+          <CodeInput recibirCodigo={codeText} validadorLexico={(codigoPlano: string) => analisisLexico(codigoPlano)} setCodeText={setCodeText} />
+        </React.Fragment>
+        :
+        <React.Fragment>
+          <button type="button" className="btn btn-danger button-action" onClick={() => setAnalizarLexico(false)}>Regresar</button>
+          <PrettyCode listadoAgrupado={listadoAgrupado} />
+          <TablesRender listadoPalabras={listadoPalabras} />
+        </React.Fragment>
+      }
     </div>
   );
 };
--- src/components/PrettyCode.tsx
+++ src/components/PrettyCode.tsx
@@ -7,28 +7,29 @@ interface PrettyCodeProps {
 }
 
 
-const PrettyCode: React.FC <PrettyCodeProps> = ({listadoAgrupado}) => {
+const PrettyCode: React.FC<PrettyCodeProps> = ({ listadoAgrupado }) => {
 
 
     return (
         <div className='code-container'>
-        <h2>Código estilizado</h2>
-        <div>
-        {listadoAgrupado.map((linea, index) => (
-                <div key={index} style={{flexDirection: 'column'}}>
-                    <span style={{marginRight: '10px'}}>{index+1}</span>
-                    {linea.map((palabra, subIndex) => (
-                            <span key={subIndex} 
-                            style={{borderBottom: palabra.esValido ? 'none' : '2px dashed red',
-                                color: palabra.esValido ? 'blue' : 'black',
-                                marginRight: '5px'
-                            }}>
+            <h2>Código</h2>
+            <div style={{ padding: '15px 15px 15px 15px', backgroundColor: '#282a3a' }}>
+                {listadoAgrupado.map((linea, index) => (
+                    <div key={index} style={{}}>
+                        <span style={{ marginRight: '10px', color: '#506882' }}>{index + 1}</span>
+                        {linea.map((palabra, subIndex) => (
+                            <span key={subIndex}
+                                style={{
+                                    borderBottom: palabra.esValido ? 'none' : '2px dashed red',
+                                    color: palabra.esValido ? '#2ba3ff' : 'white',
+                                    marginRight: '5px'
+                                }}>
                                 {palabra.palabra}
                             </span>
-                    ))}
-                </div>
-            ))}
-        </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
